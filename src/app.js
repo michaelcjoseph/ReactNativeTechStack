fdaacf77fb09d9593cb75f249374884af75f3bfc
MichaelCJoseph <mjoseph.cm@gmail.com>
2017-06-01 14:33:18 -0700

    Add header component

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => (
   <Provider store={createStore(reducers)}>
     <View>
+      <Header headerText="Tech Stack" />
       <Text>Initialize App</Text>
     </View>
   </Provider>
